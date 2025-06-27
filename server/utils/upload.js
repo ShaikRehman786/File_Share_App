e9dacb534aae9d60520c7328ddb24a91edef3815
Shaik Rehman <shaikrehman78609@gmail.com>
2025-06-27 19:51:07 +0530

    Update upload.js

--- server/utils/upload.js
+++ server/utils/upload.js
@@ -1,5 +1,17 @@
-import multer from 'multer';
+import multer from "multer";
+import path from "path";
 
-const upload = multer({ dest: 'uploads' });
+// Setup upload destination
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, `${uniqueSuffix}-${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage });
 
 export default upload;
